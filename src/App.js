1dc6c55e68917a731ccd7f13743a2e548b2b065e
Abrar Nadib <abrarnadib@gmail.com>
2021-06-08 19:04:24 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ useState, useEffect} from 'react';
+// import Products from './Components/Products/Products';
+// import Navbar from "./Components/Navbar/Navbar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {Products, Navbar} from "./Components";
+import Axios from "axios";
+
+const App = () => {
+    const [products, setProducts] = useState([]);
+    //const [cart, setCart] = useState({});
+
+    const getProduct = () => {
+        Axios({
+            method: "GET",
+            withCredentials: true,
+            url:"http://localhost:4000/products/style/Digital"      //this is API url
+        }).then((res)=>{
+            //console.log(res.data);
+            const {data} = res;
+            setProducts(data);
+        })
+    }
+
+    useEffect(()=>{
+        getProduct();
+    },[] );
+
+    console.log(products);
+
+    return (
+        <div>
+            <Navbar/>
+            <Products products={products}/>
+        </div>
+    )
 }
 
 export default App;
